06aa277b047631c44ddd84ad3ee889e09dc1ccd4
Anthony Blount <anthony00.blount00@gmail.com>
2025-08-02 19:01:14 -0500

    wae tuning

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -13,57 +13,128 @@ public class WaveManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int twoOrange = 0;
+        int fourOrange = 1;
+        int twoPink = 2;
+        int twoBlue = 3;
+        int respawnFirstBlues = 4;
+        int eightOrange = 5;
+        int fourPink = 6;
+        int twoOrangeRoundTwo = 7;
+        int eightBlue = 8;
+        int eightMoreOrange = 9;
+        int fourMorePink = 10;
+        int eightPink = 11;
+        int tenPink = 12;
+        int twentyBlue = 13;
+
+        int youShouldBeDeadByNow = 14;
+
         roundTimer = GameObject.FindWithTag("Timer").GetComponent<Timer>();
         waves =  new List<WaveSpawn>{
-            new WaveSpawn(0, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(0, 0f, spawnPoints[3], portals[0]),
+            new WaveSpawn(twoOrange, 0f, spawnPoints[0], portals[0]),
+            new WaveSpawn(twoOrange, 0f, spawnPoints[3], portals[0]),
 
-            new WaveSpawn(1, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(1, 0f, spawnPoints[1], portals[0]),
-            new WaveSpawn(1, 0f, spawnPoints[2], portals[0]),
-            new WaveSpawn(1, 0f, spawnPoints[3], portals[0]),
+            new WaveSpawn(fourOrange, 0f, spawnPoints[0], portals[0]),
+            new WaveSpawn(fourOrange, 0f, spawnPoints[1], portals[0]),
+            new WaveSpawn(fourOrange, 0f, spawnPoints[2], portals[0]),
+            new WaveSpawn(fourOrange, 0f, spawnPoints[3], portals[0]),
 
-            new WaveSpawn(2, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(2, 0f, spawnPoints[3], portals[0]),
-            new WaveSpawn(2, 0f, spawnPoints[4], portals[1]),
+            new WaveSpawn(twoPink, 0f, spawnPoints[1], portals[1]),
+            new WaveSpawn(twoPink, 0f, spawnPoints[3], portals[1]),
 
-            new WaveSpawn(3, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(3, 0f, spawnPoints[1], portals[0]),
-            new WaveSpawn(3, 0f, spawnPoints[2], portals[0]),
-            new WaveSpawn(3, 0f, spawnPoints[3], portals[0]),
-            new WaveSpawn(3, .25f, spawnPoints[4], portals[0]),
-            new WaveSpawn(3, .25f, spawnPoints[5], portals[0]),
-            new WaveSpawn(3, .25f, spawnPoints[6], portals[0]),
-            new WaveSpawn(3, .25f, spawnPoints[7], portals[0]),
+            new WaveSpawn(twoBlue, 0f, spawnPoints[0], portals[2]),
+            new WaveSpawn(twoBlue, 0f, spawnPoints[3], portals[2]),
 
-            new WaveSpawn(4, 0f, spawnPoints[2], portals[0]),
-            new WaveSpawn(4, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(4, .25f, spawnPoints[1], portals[0]),
-            new WaveSpawn(4, .25f, spawnPoints[3], portals[0]),
-            new WaveSpawn(4, .5f, spawnPoints[4], portals[0]),
-            new WaveSpawn(4, .5f, spawnPoints[6], portals[0]),
-            new WaveSpawn(4, .75f, spawnPoints[5], portals[0]),
-            new WaveSpawn(4, .75f, spawnPoints[7], portals[0]),
+            new WaveSpawn(respawnFirstBlues, 0f, spawnPoints[0], portals[1]),
+            new WaveSpawn(respawnFirstBlues, 0f, spawnPoints[3], portals[1]),
+            new WaveSpawn(respawnFirstBlues, .25f, spawnPoints[0], portals[0]),
+            new WaveSpawn(respawnFirstBlues, .25f, spawnPoints[3], portals[0]),
 
-            new WaveSpawn(5, 0f, spawnPoints[0], portals[1]),
-            new WaveSpawn(5, 0f, spawnPoints[2], portals[1]),
-            new WaveSpawn(5, .5f, spawnPoints[1], portals[1]),
-            new WaveSpawn(5, .5f, spawnPoints[3], portals[1]),
+            new WaveSpawn(eightOrange, 0f, spawnPoints[0], portals[0]),
+            new WaveSpawn(eightOrange, 0f, spawnPoints[1], portals[0]),
+            new WaveSpawn(eightOrange, 0f, spawnPoints[2], portals[0]),
+            new WaveSpawn(eightOrange, 0f, spawnPoints[3], portals[0]),
+            new WaveSpawn(eightOrange, .25f, spawnPoints[4], portals[0]),
+            new WaveSpawn(eightOrange, .25f, spawnPoints[5], portals[0]),
+            new WaveSpawn(eightOrange, .25f, spawnPoints[6], portals[0]),
+            new WaveSpawn(eightOrange, .25f, spawnPoints[7], portals[0]),
 
-            new WaveSpawn(6, 0f, spawnPoints[1], portals[2]),
-            new WaveSpawn(6, .25f, spawnPoints[2], portals[2]),
+            new WaveSpawn(fourPink, 0f, spawnPoints[0], portals[1]),
+            new WaveSpawn(fourPink, 0f, spawnPoints[2], portals[1]),
+            new WaveSpawn(fourPink, .5f, spawnPoints[1], portals[1]),
+            new WaveSpawn(fourPink, .5f, spawnPoints[3], portals[1]),
 
-            new WaveSpawn(7, 0f, spawnPoints[8], portals[0]),
-            new WaveSpawn(7, .1f, spawnPoints[9], portals[0]),
-            new WaveSpawn(7, .2f, spawnPoints[8], portals[0]),
-            new WaveSpawn(7, .3f, spawnPoints[9], portals[0]),
-            new WaveSpawn(7, .4f, spawnPoints[8], portals[0]),
-            new WaveSpawn(7, .5f, spawnPoints[9], portals[0]),
+            new WaveSpawn(twoOrangeRoundTwo, 0f, spawnPoints[8], portals[0]),
+            new WaveSpawn(twoOrangeRoundTwo, 0f, spawnPoints[9], portals[0]),
 
+            new WaveSpawn(eightBlue, 0f, spawnPoints[0], portals[2]),
+            new WaveSpawn(eightBlue, 0f, spawnPoints[1], portals[2]),
+            new WaveSpawn(eightBlue, 0f, spawnPoints[2], portals[2]),
+            new WaveSpawn(eightBlue, 0f, spawnPoints[3], portals[2]),
+            new WaveSpawn(eightBlue, .25f, spawnPoints[4], portals[2]),
+            new WaveSpawn(eightBlue, .25f, spawnPoints[5], portals[2]),
+            new WaveSpawn(eightBlue, .25f, spawnPoints[6], portals[2]),
+            new WaveSpawn(eightBlue, .25f, spawnPoints[7], portals[2]),
 
-            new WaveSpawn(10, 0f, spawnPoints[0], portals[0]),
-            new WaveSpawn(10, .25f, spawnPoints[1], portals[1]),
-            new WaveSpawn(10, .5f, spawnPoints[2], portals[2]),
+            new WaveSpawn(eightMoreOrange, 0f, spawnPoints[4], portals[0]),
+            new WaveSpawn(eightMoreOrange, 0f, spawnPoints[5], portals[0]),
+            new WaveSpawn(eightMoreOrange, 0f, spawnPoints[6], portals[0]),
+            new WaveSpawn(eightMoreOrange, 0f, spawnPoints[7], portals[0]),
+            new WaveSpawn(eightMoreOrange, .25f, spawnPoints[0], portals[0]),
+            new WaveSpawn(eightMoreOrange, .25f, spawnPoints[1], portals[0]),
+            new WaveSpawn(eightMoreOrange, .25f, spawnPoints[2], portals[0]),
+            new WaveSpawn(eightMoreOrange, .25f, spawnPoints[3], portals[0]),
+
+            new WaveSpawn(fourMorePink, 0f, spawnPoints[1], portals[1]),
+            new WaveSpawn(fourMorePink, 0f, spawnPoints[3], portals[1]),
+            new WaveSpawn(fourMorePink, .5f, spawnPoints[0], portals[1]),
+            new WaveSpawn(fourMorePink, .5f, spawnPoints[2], portals[1]),
+
+            new WaveSpawn(eightPink, 0f, spawnPoints[0], portals[1]),
+            new WaveSpawn(eightPink, 0f, spawnPoints[1], portals[1]),
+            new WaveSpawn(eightPink, 0f, spawnPoints[2], portals[1]),
+            new WaveSpawn(eightPink, 0f, spawnPoints[3], portals[1]),
+            new WaveSpawn(eightPink, .25f, spawnPoints[4], portals[1]),
+            new WaveSpawn(eightPink, .25f, spawnPoints[5], portals[1]),
+            new WaveSpawn(eightPink, .25f, spawnPoints[6], portals[1]),
+            new WaveSpawn(eightPink, .25f, spawnPoints[7], portals[1]),
+
+            new WaveSpawn(tenPink, 0f, spawnPoints[0], portals[1]),
+            new WaveSpawn(tenPink, 0f, spawnPoints[1], portals[1]),
+            new WaveSpawn(tenPink, 0f, spawnPoints[2], portals[1]),
+            new WaveSpawn(tenPink, 0f, spawnPoints[3], portals[1]),
+            new WaveSpawn(tenPink, 0f, spawnPoints[8], portals[1]),
+            new WaveSpawn(tenPink, 0f, spawnPoints[9], portals[1]),
+            new WaveSpawn(tenPink, .25f, spawnPoints[4], portals[1]),
+            new WaveSpawn(tenPink, .25f, spawnPoints[5], portals[1]),
+            new WaveSpawn(tenPink, .25f, spawnPoints[6], portals[1]),
+            new WaveSpawn(tenPink, .25f, spawnPoints[7], portals[1]),
+
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[0], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[1], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[2], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[3], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[4], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[5], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[6], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[7], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[8], portals[2]),
+            new WaveSpawn(twentyBlue, 0f, spawnPoints[9], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[0], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[1], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[2], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[3], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[4], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[5], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[6], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[7], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[8], portals[2]),
+            new WaveSpawn(twentyBlue, .5f, spawnPoints[9], portals[2]),
+
+            new WaveSpawn(youShouldBeDeadByNow, 0f, spawnPoints[0], portals[0]),
+            new WaveSpawn(youShouldBeDeadByNow, .25f, spawnPoints[1], portals[1]),
+            new WaveSpawn(youShouldBeDeadByNow, .5f, spawnPoints[2], portals[2]),
         };
     }
 
